235927db6daae8d2369cd9816b81c9ef03cf54b4
Shinon2023 <siwat61719@gmail.com>
2024-10-12 03:13:11 +0700

    finish

--- index.py
+++ index.py
@@ -1,21 +1,89 @@
 import json
+import os
+import datetime
 
-task = {"Do homework": {
-    "id": 1,
-    "date": "2022-01-01",
-    "time": "08:00",
-    "status": "pending"  
-}}
+current_time = datetime.datetime.now()
+filename = 'data.json'
 
-event_continue = True
+task = {
+    "id": 0,
+    "description": "",
+    "status": "",
+    "createdAt": "",
+    "updatedAt": "",
+}
 
-def add_task(task):
-    with open('data.json', 'w') as json_file:
-        json.dump(task, json_file, indent=4)
+while True:
+    with open(filename, 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file) 
+    event = input("Task Tracker... ")
+    event = event.split(" ");
+    if event[0] == "add":
+        task["id"] = len(data) + 1
+        task["description"] = event[1]
+        task["status"] = "todo"
+        task["createdAt"] = str(current_time)
+        task["updatedAt"] = str(current_time)
+        if os.path.exists(filename):
+            data.append(task)
+        else:
+            data = [task]
 
-
-while event_continue:
-    id = len(task) + 1
-    add_task(task)
-    print("Task added")
-    break
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            
+    elif event[0] == "mark-in-progress":
+        data[int(event[1]) - 1]["status"] = "in-progress"
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            print("Task marked in progress.")
+            
+    elif event[0] == "mark-done":
+        data[int(event[1]) - 1]["status"] = "in-progress"
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            print("Task marked in progress.")
+    
+    elif event[0] == "delete":
+        data.pop(int(event[1]) - 1)
+        for i in range(len(data)):
+            data[i]["id"] = i + 1
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            print("Task deleted.")
+    
+    elif event[0] == "update":
+        data[int(event[1]) - 1]["description"] = event[2]
+        data[int(event[1]) - 1]["updatedAt"] = str(current_time)
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)           
+            print("Task updated.")
+            
+    elif event[0] == "list":
+        description = ["ID", "Description", "Status", "Created At", "Updated At"]
+        print("-" * 119)
+        print(f"|{description[0].center(8)}|{description[1].center(30)}|{description[2].center(15)}|{description[3].center(30)}|{description[4].center(30)}|")
+        print("-" * 119)
+        if len(event) == 1:
+            for i in range(len(data)):
+                print(f"|{str(data[i]["id"]).center(8)}|{data[i]["description"][:30].center(30)}|{data[i]["status"].center(15)}|{data[i]["createdAt"].center(30)}|{data[i]["updatedAt"].center(30)}|")
+                print("-" * 119)
+        elif len(event) > 1:
+            if event[1] == "todo":
+                for i in range(len(data)):
+                    if data[i]["status"] == "todo":
+                        print(f"|{str(data[i]["id"]).center(8)}|{data[i]["description"][:30].center(30)}|{data[i]["status"].center(15)}|{data[i]["createdAt"].center(30)}|{data[i]["updatedAt"].center(30)}|")
+                        print("-" * 119)
+            if event[1] == "in-progress":
+                for i in range(len(data)):
+                    if data[i]["status"] == "in-progress":
+                        print(f"|{str(data[i]["id"]).center(8)}|{data[i]["description"][:30].center(30)}|{data[i]["status"].center(15)}|{data[i]["createdAt"].center(30)}|{data[i]["updatedAt"].center(30)}|")
+                        print("-" * 119)
+            if event[1] == "done":
+                for i in range(len(data)):
+                    if data[i]["status"] == "done":
+                        print(f"|{str(data[i]["id"]).center(8)}|{data[i]["description"][:30].center(30)}|{data[i]["status"].center(15)}|{data[i]["createdAt"].center(30)}|{data[i]["updatedAt"].center(30)}|")
+                        print("-" * 119)
+           
+    elif event[0] == "exit":
+        break
---
+++ test.py
@@ -0,0 +1,9 @@
+print("-" * 119)
+description = ["ID", "Description", "Status", "Created At", "Updated At"]
+print(f"|{description[0].center(8)}|{description[1].center(30)}|{description[2].center(15)}|{description[3].center(30)}|{description[4].center(30)}|")
+print("-" * 119)
+
+for i in range(10):
+    print(f"")
+    print("-" * 119)
+    
